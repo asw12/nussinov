347104edf479f1b847e374d6d4ccaf82b7563ac6
asw12 <asw12@berkeley.edu>
2009-10-17 16:14:37 -0700

    Trying a different numbering scheme
    Signed-off-by: asw12 <asw12@berkeley.edu>

--- nussinov.pl
+++ nussinov.pl
@@ -20,58 +20,60 @@ sub NussinovBasic
     # Init method variables
     my $seqRef = pop @_;
     my $length = length($$seqRef);
-    
+
     my %WCpairing = ( "A" => "U", "U" => "A", "G" => "C", "C" => "G" );
-    
+
     ($length >= 2 or die "Sequence is only one base long!");
 
     my(@plot, $i, $j, $k);
 
     # Know the size wanted for array; so give the memory manager a hand and allocate it
+    #  fix this
     $#plot = $length - 1;
-
-    for(0..$length-2)
+    for(0..$length-1)
     {
-        my @row = (0) x ($_+1);
+        my @row = (0) x ( $length - ($_ + 1) );
         $plot[$_] = \@row;
     }
 
-    # Numbering starts at 0,0 in the top right corner, down to $length-2 at the left and bottom ends of the plot
-    #  This way avoids negative indexes, because the code doesn't explicitly error check for the end of the arrays
+    # Numbering starts at 0,$length-1 in the top right corner, down to $length-2 at the left and bottom ends of the plot
     for($i = $length-2; $i >= 0; --$i)
     {
-        # $length-(2+$i) is the width of the current row
-        for($j = $length-(2+$i); $j >= 0; --$j)
+        for($j = 0; $j < $length - ($i - 1); ++$j)
         {
-            # Store the diagonal step (delta(i,j) + score(i-1,j-1)) in value / converts undefined to 0 in one step (save a scrap of memory)
-            my $value = $plot[$i+1][$j+1];
-            $value += ($WCpairing{substr($$seqRef, $i, 1)} eq substr($$seqRef, $length-(1+$j), 1));
-            
-            if(($plot[$i][$j+1]) && $plot[$i][$j+1] > $value)
+            # Store the diagonal step (delta(i,j) + score(i+1,j-1)) in value / converts undefined to 0 in one step (save a scrap of memory)
+            my $value = $plot[$i+1][$j-2];
+            $value += ($WCpairing{substr($$seqRef, $i, 1)} eq substr($$seqRef, $j+$i, 1));
+
+            if($plot[$i][$j-1] && $plot[$i][$j-1] > $value)
             {
-                $value = $plot[$i][$j+1];
+                $value = $plot[$i][$j-1];
             }
-            if(($plot[$i+1][$j]) && $plot[$i+1][$j] > $value)
+            if($plot[$i+1][$j-1] && $plot[$i+1][$j-1] > $value)
             {
-                $value = $plot[$i+1][$j];
+                $value = $plot[$i+1][$j-1];
             }
 
             # This slow step should really be avoided, if possible
-            if($plot[-100])
-            {
-                for($k = 0; $k < $length-(2+$i) - $j; ++$k)
-                {
-                    
-                }
-            }
-            
+            #  Make sure that it is actually possible that 
+            #if($j > $i+2 && $plot[$i+2][$j] + $plot[$i][$j-2] > $value)
+            #{
+            #    for($k = 1; $k < $j - 1; ++$k)
+            #    {
+            #        if($plot[$i][$i+$k] + $plot[$i+$k+1][$j] > $value)
+            #        {
+            #            $value = $plot[$i][$i+$k] + $plot[$i+$k+1][$j];
+            #        }
+            #    }
+            #}
+
             $plot[$i][$j] = $value;
-            
+
             #print "$i, $j, Value : $value \n";
         }
     }
 
-    print "Score: $plot[0][0]";
+    print "Score: $plot[0][$length-1]";
 }
 
 # my $start = gettimeofday;
